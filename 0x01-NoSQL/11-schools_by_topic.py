c6c5c5292f252c8ab3bcf6e4822981a322a5768d
Gideon Oba <deyonoba@gmail.com>
2024-07-16 23:15:12 +0100

    Add 11-school_topic.py file

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module contains Python Function that returns the list of school having
+a specific topic:
+    - Prototype: `def schools_by_topic(mongo_collection, topic):`
+    - `mongo_collection` will be the `pymongo` collection object
+    - `topic` (string) will be topic searched
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Gets the schools having a specific topic.
+    """
+    schools = mongo_collection.find()
+    result = {}
+
+    for school in schools:
+        if topic in school["topics"]:
+            result.update(school)
+
+    return result
